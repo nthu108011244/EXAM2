b4177df5a46b784342102d3a83cb3db2c277e54c
nthu108011244 <79581483+nthu108011244@users.noreply.github.com>
2021-05-12 15:50:24 +0800

    Add files via upload

---
+++ tfconfig.h
@@ -0,0 +1,19 @@
+#ifndef TFCONFIG_H_
+#define TFCONFIG_H_
+
+// The number of labels (without negative)
+#define label_num 2
+
+struct Config {
+
+  // This must be the same as seq_length in the src/model_train/config.py
+  const int seq_length = 64;
+
+  // The number of expected consecutive inferences for each gesture type.
+  const int consecutiveInferenceThresholds[label_num] = {20, 10};
+
+  const char* output_message[label_num] = {" (angel up) ", " (angel down) "};
+};
+
+Config config;
+#endif // TFCONFIG_H_
